f94782fdd4b5202cc77b42537a03194e1394660d
Gonzalo <carbajosa@gmail.com>
2016-04-18 00:01:21 +0200

    pagani-history-web context path removed

--- src/main/resources/static/javascript/main.js
+++ src/main/resources/static/javascript/main.js
@@ -13,11 +13,11 @@ function submitLoginForm(login)
 {		
 	if (login)
 	{
-		$("#main-form")[0].action = "/pagani-history-web/cms/login";
+		$("#main-form")[0].action = "/cms/login";
 	}
 	else
 	{
-		$("#main-form")[0].action = "/pagani-history-web/cms/login?logout";
+		$("#main-form")[0].action = "/cms/login?logout";
 	}
 
 	var csrfData = $("<input>").attr("type", "hidden").attr("name", "_csrf").val($("meta[name='_csrf']").attr("content"));
