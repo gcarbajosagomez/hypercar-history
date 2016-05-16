4cce00e2165d22caff7c46ab129d89d5abd956e4
Gonzalo <carbajosa@gmail.com>
2016-05-16 19:25:24 +0200

    randomness of the picture Ids chosen increased

--- src/main/java/com/phistory/mvc/controller/IndexController.java
+++ src/main/java/com/phistory/mvc/controller/IndexController.java
@@ -4,36 +4,22 @@ import static com.phistory.mvc.controller.BaseControllerData.INDEX_URL;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.HEAD;
 
-
-
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-
-
-
 import javax.inject.Inject;
 
-
-
-
 import lombok.extern.slf4j.Slf4j;
-
-
-
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-
-
 
 import com.phistory.mvc.springframework.view.ModelFiller;
 import com.tcp.data.model.car.Car;
@@ -65,7 +51,7 @@ public class IndexController extends BaseController implements InitializingBean
 		{		
 			carModelFiller.fillModel(model);
 			pictureModelFiller.fillModel(model);
-			model.addAttribute("carNamesToPictureIds", generateRandomCarNamesToPictureIds());	
+			model.addAttribute("carNamesToPictureIds", this.generateRandomCarNamesToPictureIds());	
 			
 			return new ModelAndView(INDEX_URL); 
 		}
@@ -92,7 +78,7 @@ public class IndexController extends BaseController implements InitializingBean
 			
 			if (this.picturesIds.size() > upperIndexLimit)
 			{				
-				randomPicIds = generateRadomPictureIdsList(this.picturesIds, upperIndexLimit);				
+				randomPicIds = this.generateRadomPictureIdsList(this.picturesIds, upperIndexLimit);				
 			}
 		}
 		
@@ -118,34 +104,12 @@ public class IndexController extends BaseController implements InitializingBean
 	private List<Long> generateRadomPictureIdsList(List<Long> pictureIds, int numOfIdsToSelect)
 	{
 		List<Long> randomPictureIds = new ArrayList<>();
-		
-		Long lowestPictureId = pictureIds.get(0);
-		Long highestPictureId = pictureIds.get(pictureIds.size() -1);			
+		int randomNumberMaxBound = pictureIds.size();
 		
 		for (int i = 0; i < numOfIdsToSelect; i++)
-		{			
-			if (highestPictureId > 0)
-			{	
-				while (true)				
-				{
-					//the limit value is exclusive, so we have to increase the number by 1
-					Long randomId = new Long(previewPictureRandomGenerator.nextInt(highestPictureId.intValue() + 1));
-					
-					//the generator has a max value, but not a min, so we need to check first
-					if (randomId >= lowestPictureId)
-					{					
-						if (pictureIds.contains(randomId) && !randomPictureIds.contains(randomId))					
-						{
-							randomPictureIds.add(Long.valueOf(randomId));
-							break;
-						}
-						else
-						{
-							randomId = new Long(previewPictureRandomGenerator.nextInt(highestPictureId.intValue() + 1));
-						}				
-					}			
-				}
-			}
+		{	
+			int randomPictureIndex = previewPictureRandomGenerator.nextInt(randomNumberMaxBound);
+			randomPictureIds.add(pictureIds.get(randomPictureIndex));
 		}
 		
 		return randomPictureIds;
@@ -154,6 +118,7 @@ public class IndexController extends BaseController implements InitializingBean
 	@Override
 	public void afterPropertiesSet() throws Exception
 	{
-		this.picturesIds = super.getPictureDao().getIdsByCarId(null);			
+		this.picturesIds = super.getPictureDao().getIdsByCarId(null);
+		Collections.shuffle(this.picturesIds);
 	}
 }
