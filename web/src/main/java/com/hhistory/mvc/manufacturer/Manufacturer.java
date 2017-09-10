237476b7503cd5d7b0b5e3b7ef6cf382df7393a6
Gonzalo Carbajosa <carbajosa@gmail.com>
2017-09-10 15:00:31 +0200

    Add Bugatti manufacturer

--- web/src/main/java/com/hhistory/mvc/manufacturer/Manufacturer.java
+++ web/src/main/java/com/hhistory/mvc/manufacturer/Manufacturer.java
@@ -1,13 +1,19 @@
 package com.hhistory.mvc.manufacturer;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Optional;
 import java.util.stream.Stream;
+
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.OBJECT;
 
 /**
  * Created by gonzalo on 6/01/17.
  */
+@JsonFormat(shape = OBJECT)
 public enum Manufacturer {
-    PAGANI("pagani", "p");
+    PAGANI("pagani", "p"),
+    BUGATTI("bugatti", "b");
 
     private String name;
     private String shortName;
@@ -31,5 +37,10 @@ public enum Manufacturer {
                                            .toLowerCase()
                                            .equals(name.toLowerCase()))
                      .findFirst();
+    }
+
+    @Override
+    public String toString() {
+        return this.getName();
     }
 }
